0451bbf454f73c57381cb2fad60c3e0d53b5baa2
Davide Porrovecchio <pdavide@gmail.com>
2019-05-16 17:58:43 +0200

    Improve master template
    - add a proper value to the html lang attribute
    - add a meta tag for the csrf token value

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,11 +1,9 @@
-
 <!doctype html>
-<html lang="it">
+<html lang="{{ app()->getLocale() }}">
 <head>
-
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title_prefix', config('bootstrap-italia.title_prefix', ''))
         @yield('title', config('bootstrap-italia.title', 'Bootstrap-Italia'))
@@ -17,7 +15,6 @@
     @yield('bootstrapitalia_css')
 
     <script>window.__PUBLIC_PATH__ = '{{ asset('vendor/bootstrap-italia/dist/fonts') }}'</script>
-
 </head>
 <body>
 @yield('body')
