1b110074326d81d6eb6a5a0e45e27c4b48328670
Xefyn <richardivan100499@gmail.com>
2020-09-23 23:19:17 +0700

    add indonesia translation

---
+++ resources/lang/id/bootstrap-italia.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'login' => 'Masuk',
+    'logout' => 'Keluar',
+    'follow_us' => 'Ikuti kami di',
+    'search' => 'Cari',
+    'toggle_navigation' => 'Perlihatkan navigasi',
+    'contacts' => 'Kontak',
+    'go_to' => 'Pindah ke halaman',
+    'newsletter' => 'Buletin',
+    'cookiebar' => [
+        'message' => 'Situs Web ini menggunakan cookie teknis, analitik, dan pihak ketiga <br>Dengan melanjutkan, Anda setuju dengan penggunaan cookie.',
+        'accept' => 'Saya menerima<span class="sr-only"> cookie</span>',
+        'preferences' => 'Oreferensi <span class="sr-only">cookie</span>',
+    ],
+    'useful_links' => 'Link yang berguna',
+];
